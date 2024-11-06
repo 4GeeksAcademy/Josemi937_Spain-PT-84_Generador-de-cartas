5e88cb00933826dade106d8d0f6c9d14f55431ba
Jose Miguel <josemi_garcia_seven@hotmail.com>
2024-11-06 18:56:42 +0000

    card generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,50 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+let suit = ["♦", "♥", "♠", "♣"];
+let numbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+let iconTop = document.querySelector("#iconTop");
+let numberValue = document.querySelector("#numberValue");
+let iconBottom = document.querySelector("#iconBottom");
+
+function generateRandomSuit() {
+  return suit[Math.floor(Math.random() * suit.length)];
+}
+
+function generateRandomNumber() {
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+function asingSuitAndNumbers() {
+  let suit = generateRandomSuit();
+  let numbers = generateRandomNumber();
+
+  iconTop.innerText = suit;
+  numberValue.innerText = numbers;
+  iconBottom.innerText = suit;
+
+  iconTop.style.color = suit === "♦" || suit === "♥" ? "red" : "black";
+  iconBottom.style.color = suit === "♦" || suit === "♥" ? "red" : "black";
+}
+console.log(asingSuitAndNumbers());
+const refreshBtn = document.getElementById("btnRefresh");
+
+function handleClick() {
+  window.location.reload();
+}
+
+refreshBtn.addEventListener("click", handleClick);
